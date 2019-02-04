774034c2cd39708b85ba7d05364f18605b92d76c
Anton Rissanen <hello@anton.fi>
2019-02-04 06:36:36 +0200

    Show sha1 sums to identify ROM version

--- _site/deploy.sh
+++ _site/deploy.sh
@@ -1,2 +1,3 @@
+find ./roms | grep '\.nes$' | xargs sha1sum | awk -F '[ /]' '{ ORS=","; OFS=":";print "\"" $5 "\"", "\"" $1 "\""}' | echo "{$(cat | sed s'/.$//')}" > _data/roms_sha1sums.json
 bundle exec jekyll build
 git subtree push --prefix _site gh-pages master
